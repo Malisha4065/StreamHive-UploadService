74710217cfba3e1e74b4c1a937527903f4c0a97e
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2025-08-24 02:25:00 +0530

    fix warning message

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const healthRoutes = require('./routes/health')
 const errorHandler = require('./middleware/errorHandler')
 
 const app = express()
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3001
 
 // Security middleware
